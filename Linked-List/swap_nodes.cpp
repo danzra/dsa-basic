296c468e3ec5ff9e4580a60886973e94a7cea625
arpit goswami <87869007+danzra@users.noreply.github.com>
2022-02-27 17:45:33 +0530

    Add files via upload

--- Linked-List/swap_nodes.cpp
+++ Linked-List/swap_nodes.cpp
@@ -47,6 +47,8 @@ void print(node *head)
 	print(head->next);
 
 }
+//swap data
+/*
 node *swap_nodes(node *head , int m ,int n)
 {
 	node *p=head,*q=head;
@@ -66,6 +68,56 @@ node *swap_nodes(node *head , int m ,int n)
     
     return head;
 
+}
+*/
+node *swap_nodes(node *head , int m , int n)
+{
+	node *p1 = NULL;
+	node *p2 = NULL;
+	node *c1 = head;
+	node *c2 = head;
+	node *temp =head;
+	node *prev = NULL;
+	int count = 0;
+	while(temp!=NULL)
+	{
+		if(count == m)
+		{
+			c1 = temp;
+			p1 = prev;
+		}
+		else if(count == n)
+		{
+			c2 = temp;
+			p2 = prev;
+		}
+		prev = temp;
+		temp = temp->next;
+		count++;
+
+	}
+
+	if(p1!=NULL)
+	{
+		p1->next = c2;
+	}
+	else
+	{
+		head= c2;
+	}
+
+	if(p2!=NULL)
+	{
+		p2->next = c1;
+	}
+	else
+	{
+		head = c1;
+	}
+	node *p = c2->next;
+	c2->next = c1->next;
+	c1->next = p;
+	return head;
 }
 
 int main()
